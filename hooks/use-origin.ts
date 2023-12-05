461f527078c26a6e2b94e2a2d3dfd52ab8ebb11a
Lien <hwenyi@outlook.my>
2023-12-05 09:17:59 +0800

    useOrigin钩子，读取页面url

---
+++ hooks/use-origin.ts
@@ -0,0 +1,14 @@
+//读取当前url
+
+import { useEffect, useState } from "react"
+
+export const useOrigin = () => {
+  const [mounted , setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  },[])
+
+  //读取页面url，防止客户端还没渲染window，服务端确提前渲染完成的报错
+  const origin = typeof window !== "undefined" && window.location.origin ? window.location.origin : ""
+}
